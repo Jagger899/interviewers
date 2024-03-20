e6ad6b92a30347394a5146559e8f5d35cf125a9a
VUE Дмитро Гулак <stajectgamer@gmail.com>
2024-03-20 23:23:49 +0200

    main: исправление замечаний по модульной системе

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,15 +1,15 @@
-export const burgerFunction = () => {
+export function burgerFunction ()  {
   const burger = document.querySelector(".header__burger-icon");
   const background = document.querySelector(".header__burger-background");
-  const botton = document.querySelector(".header__button");
+  const bottom = document.querySelector(".header__button");
   const logo = document.querySelector(".header__logo");
   const language = document.querySelector(".header__language");
 
   burger.addEventListener("click", function () {
     burger.classList.toggle("header__burger-icon_active");
     background.classList.toggle("header__burger-background_active");
-    botton.classList.toggle("header__button_active");
+    bottom.classList.toggle("header__button_active");
     logo.classList.toggle("header__logo_active");
     language.classList.toggle("header__language_active");
   });
-};
+}
