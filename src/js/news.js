9dcf047920ad95d03e584cd557225a1a6f8cdaa5
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-03-27 22:03:41 +0200

    improved the news section

--- src/js/news.js
+++ src/js/news.js
@@ -1,5 +1,9 @@
 import { burgerFunction } from "./components/header";
+import { colorFooter } from "./news/news-communication";
+import { filterPage } from "./news/news-cards";
 
 document.addEventListener("DOMContentLoaded", async function () {
-  burgerFunction();
+    burgerFunction();
+    colorFooter();
+    filterPage();
 });
