be723fa53e6cf065b979ceb52f7d838e84c27868
Jagger899 <romanskarb2@ukr.net>
2024-04-04 17:02:34 +0300

    part of modal was added

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,11 @@ import { burgerFunction } from "./components/header";
 import { mainSwiper } from "./components/swiper";
 import { dropdownWork } from "./components/dropdown";
 import { preloaderLoad } from "./components/preloader";
+import { modalOpen } from "./components/modal";
 
 document.addEventListener("DOMContentLoaded", async function () {
-    preloaderLoad();
+  preloaderLoad();
+  modalOpen()
     burgerFunction();
     mainSwiper();
     dropdownWork();
