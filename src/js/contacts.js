cab32e6b7664e59e01632b7cb845cc0f93fa7ca6
Jagger899 <romanskarb2@ukr.net>
2024-04-04 19:14:30 +0300

    one-news preload was fixed

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,8 +1,10 @@
 import { burgerFunction } from "./components/header";
 import { contactsFunction } from "./components/contacts";
 import { preloaderLoad } from "./components/preloader";
+import { modalOpen } from "./components/modal";
 
 document.addEventListener("DOMContentLoaded", async function () {
+  modalOpen();
   preloaderLoad();
     contactsFunction();
     burgerFunction();
--- src/js/one-news.js
+++ src/js/one-news.js
@@ -1,7 +1,9 @@
 import { burgerFunction } from "./components/header";
 import { newsSwiper } from "./one-news/news-swiper";
+import { preloaderLoad } from "./components/preloader";
 
 document.addEventListener("DOMContentLoaded", async function () {
+  preloaderLoad();
     burgerFunction();
     newsSwiper();
 });
