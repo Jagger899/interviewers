195af8a6e6a4a60f3e03e42e2eaaaa3d08ad9c42
Jagger899 <romanskarb2@ukr.net>
2024-04-04 18:26:17 +0300

    paths main was changed

--- src/js/about.js
+++ src/js/about.js
@@ -1,4 +1,6 @@
 import { burgerFunction } from "./components/header";
+import { preloaderLoad } from "./components/preloader";
 document.addEventListener("DOMContentLoaded", async function () {
+  preloaderLoad();
     burgerFunction();
 });
--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,7 +1,9 @@
 import { burgerFunction } from "./components/header";
 import { contactsFunction } from "./components/contacts";
+import { preloaderLoad } from "./components/preloader";
 
 document.addEventListener("DOMContentLoaded", async function () {
+  preloaderLoad();
     contactsFunction();
     burgerFunction();
 });
