94f03e27cdb5abac0fbe677a0affdcd85e76321e
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-04-07 15:40:09 +0300

    add achievements and philosophy section

--- src/js/about.js
+++ src/js/about.js
@@ -1,7 +1,9 @@
 import { preloaderLoad } from "./components/preloader";
 import { burgerFunction } from "./components/header";
+import { swiperPhilosophy } from "./about/swiper-philosophi";
 
 document.addEventListener("DOMContentLoaded", async function () {
     preloaderLoad();
     burgerFunction();
+    swiperPhilosophy();
 });
--- src/js/news.js
+++ src/js/news.js
@@ -2,7 +2,6 @@ import { preloaderLoad } from "./components/preloader";
 import { burgerFunction } from "./components/header";
 import { colorFooter } from "./news/news-communication";
 import { filterPage } from "./news/news-cards";
-
 document.addEventListener("DOMContentLoaded", async function () {
     preloaderLoad();
     burgerFunction();
