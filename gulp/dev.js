5f385a8bcfece392a86e36694412c78b30bd5c4d
Jagger899 <romanskarb2@ukr.net>
2024-04-11 12:09:35 +0300

    gulp was changed

--- gulp/dev.js
+++ gulp/dev.js
@@ -1,8 +1,13 @@
 const gulp = require("gulp");
 const { src, dest, task, watch, parallel, series } = require("gulp");
 const scss = require("gulp-sass")(require("sass"));
+const csso = require("gulp-csso");
+const webpCss = require("gulp-webp-css");
 const sassGlob = require("gulp-sass-glob");
 const fileInclude = require("gulp-file-include");
+const webpHtml = require("gulp-webp-html");
+const autoprefixer = require("gulp-autoprefixer");
+const htmlclean = require("gulp-htmlclean");
 const fileIncludeSettings = { prefix: "@@", basepath: "@file" };
 const server = require("gulp-server-livereload");
 const serverSettings = { livereload: true, open: true };
@@ -10,15 +15,16 @@ const clean = require("gulp-clean");
 const fs = require("fs");
 const sourseMaps = require("gulp-sourcemaps");
 const plumber = require("gulp-plumber");
+
 const notify = require("gulp-notify");
 const plumberNotify = function (title) {
-  return {
-    errorHandler: notify.onError({
-      title: title,
-      message: "ERROR <%= error.message %>",
-      sound: false,
-    }),
-  };
+    return {
+        errorHandler: notify.onError({
+            title: title,
+            message: "ERROR <%= error.message %>",
+            sound: false,
+        }),
+    };
 };
 const webpack = require("webpack-stream");
 const imagemin = require("gulp-imagemin");
@@ -26,70 +32,76 @@ const changed = require("gulp-changed");
 const webp = require("gulp-webp");
 
 task("html:dev", function () {
-  return src(["src/html/**/*.html", "!src/html/blocks/*.html"])
-    .pipe(changed("build/", { hasChanged: changed.compareContents }))
-    .pipe(plumber(plumberNotify("HTML")))
-    .pipe(fileInclude(fileIncludeSettings))
-    .pipe(dest("build/"));
+    return (
+        src(["src/html/**/*.html", "!src/html/blocks/*.html"])
+            .pipe(changed("build/", { hasChanged: changed.compareContents }))
+            // .pipe(changed("build"))
+            .pipe(plumber(plumberNotify("HTML")))
+            .pipe(fileInclude(fileIncludeSettings))
+            // .pipe(webpHtml())
+            .pipe(htmlclean())
+            .pipe(dest("build/"))
+    );
 });
 
 task("sass:dev", function () {
-  return src("src/scss/*.scss")
-    .pipe(changed("build/css"))
-    .pipe(plumber(plumberNotify("SASS")))
-    .pipe(sourseMaps.init())
-    .pipe(sassGlob())
-    .pipe(scss())
-    .pipe(sourseMaps.write())
-    .pipe(dest("build/css/"));
+    return src("src/scss/*.scss")
+        .pipe(changed("build/css"))
+        .pipe(plumber(plumberNotify("SASS")))
+        .pipe(sourseMaps.init())
+        // .pipe(autoprefixer())
+        .pipe(sassGlob())
+        // .pipe(webpCss())
+        .pipe(scss())
+        // .pipe(csso())
+        .pipe(sourseMaps.write())
+        .pipe(dest("build/css/"));
 });
 
 task("images:dev", function () {
-  return src("src/images/**/*")
-      .pipe(changed("build/images"))
-      .pipe(webp())
-      .pipe(dest("build/images"))
-      .pipe(src("src/images/**/*"))
-      .pipe(changed("docs/images"))
-      .pipe(imagemin({ verbose: true }))
-      .pipe(dest("build/images"));
+    return src("src/images/**/*")
+        .pipe(changed("build/images"))
+        .pipe(webp())
+        .pipe(dest("build/images"))
+        .pipe(src("src/images/**/*"))
+        .pipe(changed("build/images"))
+        .pipe(imagemin({ verbose: true }))
+        .pipe(dest("build/images"));
 });
 
 task("fonts:dev", function () {
-  return src("src/fonts/**/*")
-    .pipe(changed("build/fonts/"))
-    .pipe(dest("build/fonts"));
+    return src("src/fonts/**/*")
+        .pipe(changed("build/fonts/"))
+        .pipe(dest("build/fonts"));
 });
 
 task("js:dev", function () {
-  return (
-    src("./src/js/*.js")
-      .pipe(changed("build/js"))
-      .pipe(plumber(plumberNotify("JS")))
-      // .pipe(babel())
-      .pipe(webpack(require("../webpack.config.js")))
-      .pipe(dest("build/js"))
-  );
+    return (
+        src("./src/js/*.js")
+            .pipe(changed("build/js"))
+            .pipe(plumber(plumberNotify("JS")))
+            // .pipe(babel())
+            .pipe(webpack(require("../webpack.config.js")))
+            .pipe(dest("build/js"))
+    );
 });
 
 task("server:dev", function () {
-  return src("build/").pipe(server(serverSettings));
+    return src("build/").pipe(server(serverSettings));
 });
 
 task("clean:dev", function (done) {
-  if (fs.existsSync("build")) {
-    return src("build", { read: false }).pipe(clean({ force: true }));
-  }
+    if (fs.existsSync("build")) {
+        return src("build", { read: false }).pipe(clean({ force: true }));
+    }
 
-  done();
+    done();
 });
 
 task("watch:dev", function () {
-  watch("src/scss/**/*.scss", parallel("sass:dev"));
-  watch("src/**/*.html", parallel("html:dev"));
-  watch("src/images/**/*", parallel("images:dev"));
-  watch("src/fonts/**/*", parallel("fonts:dev"));
-  watch("src/js/**/*.js", parallel("js:dev"));
+    watch("src/scss/**/*.scss", parallel("sass:dev"));
+    watch("src/**/*.html", parallel("html:dev"));
+    watch("src/images/**/*", parallel("images:dev"));
+    watch("src/fonts/**/*", parallel("fonts:dev"));
+    watch("src/js/**/*.js", parallel("js:dev"));
 });
-
-
--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,25 @@ require("./gulp/dev.js");
 require("./gulp/docs.js");
 
 task(
-  "default",
-  series(
-    "clean:dev",
-    parallel("html:dev", "sass:dev", "images:dev", "fonts:dev", "js:dev"),
-    parallel("server:dev", "watch:dev")
-  )
+    "default",
+    series(
+        "clean:dev",
+        parallel("html:dev", "sass:dev", "images:dev", "fonts:dev", "js:dev"),
+        parallel("server:dev", "watch:dev"),
+    ),
 );
 
 task(
-  "docs",
-  series(
-    "clean:docs",
-    parallel("html:docs", "sass:docs", "images:docs", "fonts:docs", "js:docs"),
-    parallel("server:docs")
-  )
+    "docs",
+    series(
+        "clean:docs",
+        parallel(
+            "html:docs",
+            "sass:docs",
+            "images:docs",
+            "fonts:docs",
+            "js:docs",
+        ),
+        parallel("server:docs"),
+    ),
 );
-
