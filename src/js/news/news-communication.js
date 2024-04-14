00719cc0c8d1c6d817cc2e9f33165fe58fc3707b
VitaliySamborskiy <vitaliysamborsky2002@gmail.com>
2024-04-14 16:03:31 +0300

    big fix bugs

--- src/js/news/news-communication.js
+++
@@ -1,6 +0,0 @@
-export const colorFooter = () => {
-    let colorText = document.querySelectorAll(".footer__text-trriger");
-    colorText.forEach((add) => {
-        add.classList.add("footer__white-text");
-    });
-};
