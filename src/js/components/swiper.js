9c5056323d52b46f94f0d23c8f053e0bce331b60
Vitaliy <vitaliysamborsky2002@gmail.com>
2024-04-16 23:35:02 +0300

    fix bugs

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -7,26 +7,27 @@ import "swiper/css/pagination";
 Swiper.use([Navigation, Pagination]);
 
 export const mainSwiper = function () {
-  const mainSwiper = new Swiper(".promo__swiper", {
-      spaceBetween: 0,
-      slidesPerView: "auto",
-      navigation: {
-          nextEl: ".promo__button-next",
-          prevEl: ".promo__button-prev",
-      },
-      pagination: {
-          el: ".promo__swiper-pagination",
-          type: "progressbar",
-      },
-      scrollbar: {
-          el: ".promo__swiper-scrollbar",
-      },
-  });
+    const mainSwiper = new Swiper(".swiper", {
+        spaceBetween: 0,
+        slidesPerView: "auto",
+        loop: true,
+        navigation: {
+            nextEl: ".button-next",
+            prevEl: ".button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "progressbar",
+        },
+        scrollbar: {
+            el: ".swiper-scrollbar",
+        },
+    });
 
-  let progressNumber = document.querySelector(".promo__number_left");
+    let progressNumber = document.querySelector(".number_change");
 
-  mainSwiper.on("slideChange", function () {
-      let activeSlide = this.activeIndex + 1;
-      progressNumber.textContent = "0" + activeSlide;
-  });
-}
+    mainSwiper.on("slideChange", function () {
+        let activeSlide = this.activeIndex + 1;
+        progressNumber.textContent = "0" + activeSlide;
+    });
+};
--- src/js/index.js
+++ src/js/index.js
@@ -2,23 +2,13 @@ import { burgerFunction } from "./components/header";
 import { mainSwiper } from "./components/swiper";
 import { dropdownWork } from "./components/dropdown";
 import { preloaderLoad } from "./components/preloader";
-// import { modalOpen } from "./components/modal";
+import { modalOpen } from "./components/modal";
 
 
 document.addEventListener("DOMContentLoaded", async function () {
     preloaderLoad();
-    // modalOpen();
+    modalOpen();
     burgerFunction();
     mainSwiper();
     dropdownWork();
-});
-
-const header = document.querySelector(".header");
-
-window.addEventListener("scroll", function () {
-    if (this.window.scrollY > 0) {
-        header.classList.add("header_black");
-    } else {
-        header.classList.remove("header_black");
-    }
 });
